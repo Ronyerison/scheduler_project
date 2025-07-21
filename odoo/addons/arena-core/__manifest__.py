d31e1d529f494abaa473fdfa2ec73aa76767a8e3
Ronyerison Dantas Braga <ronyerisonbraga@greenwave.tec.br>
2025-07-21 16:51:09 -0300

    finalizando configuracoes do projeto

--- odoo/addons/arena-core/__manifest__.py
+++ odoo/addons/arena-core/__manifest__.py
@@ -3,11 +3,15 @@
     'version': '1.0',
     'author': 'RDB Software Foundation',
     'category': 'Tools',
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'web',
+    ],
     'license': 'LGPL-3',
     'data': [
         'security/ir.model.access.csv',
         'views/arena_model_views.xml',
+        'views/menu.xml',
     ],
     'installable': True,
     'application': True,
